dcd53933b2b18d2b48bac0a0d9bdc02a3f579bdd
Ethan Patterson <159935067+EthanMPatterson@users.noreply.github.com>
2024-02-28 22:16:46 -0500

    Final Commit

--- Test.py
+++
@@ -1,7 +0,0 @@
-def negate(nums):
-    for i in range(len(nums)):
-        if nums [i] > 0:
-            nums [i] = -nums[i]
-    print(nums)
-
-negate([100, 534, 32, 15, 77, 222, 788, 345, 75645, 22])
